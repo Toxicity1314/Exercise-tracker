4e82fe15e8c15c75e8b7ae1dc9c49e4df4fd365d
Sean Welsh <s.welsh1314@gmail.com>
2023-02-16 14:13:39 -0500

    formatted code for better readability and removed unused items

--- client/src/components/WorkoutsPage.js
+++ client/src/components/WorkoutsPage.js
@@ -1,29 +1,25 @@
-
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import WorkoutCard from "./WorkoutCard";
 import { Grid } from "semantic-ui-react";
 
-
 function WorkoutsPage() {
-  const [workouts, setWorkouts] = useState([])
+  const [workouts, setWorkouts] = useState([]);
 
-  useEffect(()=>{
-     fetch("/blueprints")
-    .then(res=>res.json())
-    .then(data => setWorkouts(data))
-},[])
-const workoutList = workouts.map(workout => <WorkoutCard key={workout.id} workout={workout}/>)
-
+  useEffect(() => {
+    fetch("/blueprints")
+      .then((res) => res.json())
+      .then((data) => setWorkouts(data));
+  }, []);
+  const workoutList = workouts.map((workout) => (
+    <WorkoutCard key={workout.id} workout={workout} />
+  ));
 
   return (
     <div>
       <h1 className="header"> Please Select A Workout</h1>
-      <Grid>
-     {workoutList}
-     </Grid>
+      <Grid>{workoutList}</Grid>
     </div>
   );
 }
-
 
 export default WorkoutsPage;
