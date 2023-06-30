2d5b3ecde44a5f3be3709d526df2815f57269cb1
Sean Welsh <s.welsh1314@gmail.com>
2023-06-29 21:15:33 -0400

    updated ruby added api infor to readme

--- client/src/components/WorkoutsPage.js
+++ client/src/components/WorkoutsPage.js
@@ -4,6 +4,7 @@ import { Grid } from "semantic-ui-react";
 
 function WorkoutsPage() {
   const [workouts, setWorkouts] = useState([]);
+  const [setsNumber, setSetsNumber] = useState(1)
 
   useEffect(() => {
     fetch("/blueprints")
@@ -17,6 +18,9 @@ function WorkoutsPage() {
   return (
     <div>
       <h1 className="header"> Please Select A Workout</h1>
+      <h2 className="header">Please select number of sets
+      <input value={setsNumber} onChange={(e)=>setSetsNumber(e.target.value)}></input>
+      </h2>
       <Grid>{workoutList}</Grid>
     </div>
   );
