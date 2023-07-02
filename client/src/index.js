c7b6a47bc3693d4f0ed78996a6ec8bcf8f49700a
Matt Hunt <mstephenhunt@gmail.com>
2023-07-02 11:30:01 -0400

    rendering

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import "semantic-ui-css/semantic.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
