a3e3a4651b8ab27a52d1882b5cf412c46dba1476
Matt Hunt <mstephenhunt@gmail.com>
2023-07-05 13:54:52 -0400

    navbar cleanup

--- client/src/components/BlueprintSelection.tsx
+++ client/src/components/BlueprintSelection.tsx
@@ -1,5 +1,3 @@
 import React from "react";
 
-export default function BlueprintSelection() {
-  
-}
+export default function BlueprintSelection() {}
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 
 export default function NavBar({ user, setUser }) {
   async function handleLogout() {
     await fetch("/logout", {
       method: "DELETE",
-    })
+    });
 
     await setUser(null);
   }
@@ -23,37 +23,39 @@ export default function NavBar({ user, setUser }) {
       <AppBar position="static">
         <Toolbar
           sx={{
-            display: 'flex',
-            justifyContent: 'center',
+            display: "flex",
+            justifyContent: "center",
           }}
         >
-          <Box sx={{ 
-            display: 'flex',
-            flexDirection: 'row',
-            gap: '1rem',
-            }}>
-            <Button 
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: "1rem",
+            }}
+          >
+            <Button
               component={Link}
               to="/workouts"
-              color="inherit" 
+              color="inherit"
               variant="outlined"
               size="large"
             >
               Home
             </Button>
-            <Button 
+            <Button
               component={Link}
               to="/currentworkout"
-              color="inherit" 
+              color="inherit"
               variant="outlined"
               size="large"
             >
               Current Workout
             </Button>
-            <Button 
+            <Button
               component={Link}
               to="/"
-              color="inherit" 
+              color="inherit"
               variant="outlined"
               size="large"
               onClick={handleLogout}
@@ -64,5 +66,5 @@ export default function NavBar({ user, setUser }) {
         </Toolbar>
       </AppBar>
     </Box>
-  )
+  );
 }
