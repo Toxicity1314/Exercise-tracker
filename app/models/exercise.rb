3ee408cd3198ad858ba03eccfd03adb73c7846b3
Sean Welsh <s.welsh1314@gmail.com>
2023-07-06 20:58:46 -0400

    updated schema started excise creation

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,19 +1,23 @@
 class Exercise < ApplicationRecord
   belongs_to :blueprint
-  belongs_to :user
   has_many :exercise_sets, dependent: :destroy
 
   def self.create_exercise(blueprint_id, workout_id, sets, user_id)
+    sets = 3
     @blueprint = Blueprint.find(blueprint_id)
     exercises = @blueprint.exercises.where(workout_id: nil)
     exercises.each do |exercise|
+      weight = 5
+      reps = 8
       new_exercise =
         Exercise.create!(
           name: exercise["name"],
           instructions: exercise["instructions"],
           blueprint_id: exercise["blueprint_id"],
           pic_url: exercise["pic_url"],
-          workout_id: workout_id
+          workout_id: workout_id,
+          weight: weight,
+          reps: reps
         )
       sets.times do
         ExerciseSet.create_exercise_sets new_exercise["id"], user_id
