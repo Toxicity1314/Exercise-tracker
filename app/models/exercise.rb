f01c12414664d5ccbea50fc6c22b83ce9e0c3f6d
Sean Welsh <s.welsh1314@gmail.com>
2023-07-27 15:01:42 -0400

    merged to main

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -25,22 +25,18 @@ class Exercise < ApplicationRecord
     end
   end
 
-def set_weight_and_reps(user_id)
-    # debugger
+  def set_weight_and_reps(user_id)
     exercise =
       Exercise.where(name: name, user_id: user_id).order(:created_at).last
-    if exercise
-      sets =
-        ExerciseSet.where(exercise_id: exercise.id).where.not(completed_at: nil)
-      if sets.all? { |set|
-           set[:weight] == exercise.weight && set[:reps] == exercise.reps
-         }
-        return { weight: exercise.weight, reps: exercise.reps += 1 }
-      else
-        return { weight: 100, reps: 100 }
-      end
+    return { weight: 20, reps: 80 } if !exercise
+    sets =
+      ExerciseSet.where(exercise_id: exercise.id).where.not(completed_at: nil)
+    if sets.all? { |set|
+         set[:weight] == exercise.weight && set[:reps] == exercise.reps
+       }
+      return { weight: exercise.weight, reps: exercise.reps += 1 }
     else
-      return { weight: 20, reps: 80 }
+      return { weight: 100, reps: 100 }
     end
   end
 end
